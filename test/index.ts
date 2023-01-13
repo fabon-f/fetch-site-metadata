0465ffe904a1c2da43f70b33ffd2e931c7d580b8
fabon <syobon.hinata.public@gmail.com>
2023-01-14 08:29:22 +0900

    Add test

--- test/index.ts
+++ test/index.ts
@@ -1,6 +1,32 @@
 import test from 'ava'
+import listen from 'test-listen'
+import { createServer } from 'node:http'
 import fetchSiteMetadata from '../src/index.js'
 
-test('fn() doesn\'t throw error', t => {
-	t.notThrows(() => fetchSiteMetadata())
+const testServer = createServer(async (req, res) => {
+  if (req.url === undefined) { throw new Error('req.url is undefined') }
+  if (req.url === '/slow') {
+    res.write('<title>title</title><div>')
+    await new Promise(resolve => setTimeout(resolve, 5000))
+    res.end('</div></html>')
+    return
+  }
+  res.end('')
+})
+
+const url = await listen(testServer)
+
+test('Interrupt request when <body> starts', async t => {
+  t.timeout(300)
+  const res = await fetchSiteMetadata(new URL('/slow', url).toString())
+  t.deepEqual(res, {
+    title: 'title',
+    description: undefined,
+    icon: undefined,
+    image: undefined
+  })
+})
+
+test.after(() => {
+  testServer.close()
 })
