cdff25a33447fdf989e6680a6e273f74aa3fbc02
fabon <syobon.hinata.public@gmail.com>
2023-01-14 07:33:36 +0900

    Make icon and image URL absolute

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ import fetch from 'node-fetch'
 
 import extractMetadata from './extract-metadata.js'
 
+const fullUrl = (relativeUrl: string, baseUrl: string) => (
+  new URL(relativeUrl, baseUrl).toString()
+)
+
 export default async function fetchSiteMetadata(url: string) {
   const controller = new AbortController()
   const response = await fetch(url, {
@@ -10,5 +14,10 @@ export default async function fetchSiteMetadata(url: string) {
   if (!response.body) { throw new Error('response.body') }
   const metadata = await extractMetadata(response.body)
   controller.abort()
-  return metadata
+
+  return {
+    ...metadata,
+    icon: typeof metadata.icon === 'string' ? fullUrl(metadata.icon, url) : undefined,
+    image: typeof metadata.image === 'string' ? fullUrl(metadata.image, url) : undefined,
+  }
 }
